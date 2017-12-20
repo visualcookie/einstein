7d7118345095cb1f729a0d5aec66704687de8bd0
visualcookie <hello@visualcookie.me>
2017-12-20 08:25:07 +0100

    adds webpack for building

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path              = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/assets/js/app.js', './src/assets/scss/main.scss'],
+
+  output: {
+    path: path.resolve('dist'),
+    filename: 'js/[name].bundle.js'
+  },
+
+  watch: true,
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('css/[name].css')
+  ]
+};
